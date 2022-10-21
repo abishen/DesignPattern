aa8344f815b9a011a9a91d931fa119422d494f80
Dillon Dhayanandan <dillon.dhayanandan@royalmail.com>
2022-10-21 19:43:51 +0100

    refactored the structure of the Folder Rule engine

--- DesignPattern/RuleEngine-CardValidation/CardType.cs
+++ DesignPattern/RuleEngine-CardValidation/CardType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DesignPattern.RuleEngine_CardValidation
+﻿namespace DesignPattern.RuleEngine_CardValidation
 {
     public enum CardType : byte
     {
